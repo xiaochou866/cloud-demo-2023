810110e0236240da657a1ccdf7c7c107c369fd4e
Joker <1574427203@qq.com>
2023-04-23 10:55:24 +0800

    gateway

--- user-service/src/main/java/cn/itcast/user/web/UserController.java
+++ user-service/src/main/java/cn/itcast/user/web/UserController.java
@@ -39,8 +39,15 @@ public class UserController {
      * @param id 用户id
      * @return 用户
      */
+    //@GetMapping("/{id}")
+    //public User queryById(@PathVariable("id") Long id, String truth) {
+    //    return userService.queryById(id);
+    //}
+
     @GetMapping("/{id}")
-    public User queryById(@PathVariable("id") Long id) {
+    public User queryById(@PathVariable("id") Long id,
+                          @RequestHeader(value = "Truth", required = false) String truth) {
+        System.out.println("truth: "+ truth);
         return userService.queryById(id);
     }
 }
